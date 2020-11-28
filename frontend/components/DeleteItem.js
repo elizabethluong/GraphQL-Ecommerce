a8582bbddc9bf7d8de1bfa3647986d5d4637de0f
Elizabeth luong <elizabethluong@live.co.uk>
2020-11-28 15:11:41 +0000

    update cache in front end to matchserver

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
-import { deleteItem } from "../../backend/src/resolvers/Mutation";
+import { ALL_ITEMS_QUERY } from "./Items";
 
 const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -10,12 +10,22 @@ const DELETE_ITEM_MUTATION = gql`
     }
   }
 `;
+
 class DeleteItem extends Component {
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
+    data.items = data.items.filter(
+      (item) => item.id !== payload.data.deleteItem.id
+    );
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
-        variables={{ id: this.props.id }}>
+        variables={{ id: this.props.id }}
+        update={this.update}>
         {(deleteItem, { error }) => (
           <button
             onClick={() => {
@@ -30,4 +40,5 @@ class DeleteItem extends Component {
     );
   }
 }
+
 export default DeleteItem;
